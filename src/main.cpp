8b05bb7f6e27488fbe3094f3d9e8fd1b5c01051f
tedomi2705 <hongducthbk123@gmail.com>
2022-05-21 20:29:40 +0700

    Add setting screen

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,17 @@ int main(int argc, char* argv[]) {
 
         SDL_Event e;
         SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+
         Stage stage = MENU;
+        Music BGM(setting["path"]["menuBGM"]);
+        BGM.playMusic(-1);
+        // NOTE: Load setting
+        bool showGUI = setting["setting"]["ShowGUI"][0] - '0';
+        char skillBinding = setting["keybinding"]["skill"][0];
+        char flashBinding = setting["keybinding"]["flash"][0];
+        Music BGM(setting["path"]["menuBGM"]);
+        BGM.playMusic(-1);
         while (stage != QUIT) {
-            Music BGM(setting["path"]["menuBGM"]);
-            BGM.playMusic(-1);
             if (stage == MENU) {
                 LTexture background(gRenderer, setting["path"]["menu"]);
                 LTexture yasuo(gRenderer, setting["path"]["yasuo"]);
@@ -61,13 +68,88 @@ int main(int argc, char* argv[]) {
             }
             if (stage == OPTION_MENU) {
                 /* code */
+                clog << "Option screen" << endl;
+                LTexture background(gRenderer, setting["path"]["menu"]);
+                LTexture text(gRenderer, "CHANGE KEY BINDING: ", Arial, {0xff, 0xff, 0xff});
+                LTexture ui(gRenderer, "UI: ", Arial, {0xff, 0xff, 0xff});
+
+                string skillKeyString(1, toupper(skillBinding));
+                LTexture skillKey(gRenderer, skillKeyString, Arial, {0xff, 0xff, 0xff});
+
+                string flashKeyString(1, toupper(flashBinding));
+                LTexture flashKey(gRenderer, flashKeyString, Arial, {0xff, 0xff, 0xff});
+
+                Button menu(gRenderer, setting["path"]["menuButton"]);
+                menu.setRect(51, 29, 277, 71);
+
+                Button skill(gRenderer, setting["path"]["skillBinding"]);
+                skill.setRect(210, 255, 132, 132);
+
+                Button flash(gRenderer, setting["path"]["flashBinding"]);
+                flash.setRect(456, 255, 132, 132);
+
+                string showGUIBtnPath;
+                showGUI ? showGUIBtnPath = "onButton" : showGUIBtnPath = "offButton";
+                Button showGUIBtn(gRenderer, setting["path"][showGUIBtnPath]);
+                showGUIBtn.setRect(749, 255, 132, 132);
+
+                LTexture settingFrame(gRenderer, setting["path"]["settingFrame"]);
                 while (stage == OPTION_MENU) {
                     SDL_RenderClear(gRenderer);
                     while (SDL_PollEvent(&e) != 0) {
                         if (e.type == SDL_QUIT) {
                             stage = QUIT;
                         }
+                        if (e.type == SDL_MOUSEBUTTONDOWN) {
+                            if (menu.onHover()) {
+                                stage = MENU;
+                            }
+                            if (showGUIBtn.onHover()) {
+                                if (showGUI) {
+                                    showGUI = false;
+                                    showGUIBtn.loadFromFile(setting["path"]["offButton"]);
+                                    setting["setting"]["ShowGUI"][0] = '0';
+                                    settingFile.write(setting);
+                                } else {
+                                    showGUI = true;
+                                    showGUIBtn.loadFromFile(setting["path"]["onButton"]);
+                                    setting["setting"]["ShowGUI"][0] = '1';
+                                    settingFile.write(setting);
+                                }
+                            }
+                        }
+                        if (e.type == SDL_KEYDOWN) {
+                            if (skill.onHover()) {
+                                skillBinding = e.key.keysym.sym;
+                                skillKeyString[0] = toupper(skillBinding);
+                                setting["keybinding"]["skill"][0] = skillBinding;
+                                skillKey.loadFromRenderedText(skillKeyString, Arial,
+                                                              {0xff, 0xff, 0xff});
+                                settingFile.write(setting);
+                            }
+                            if (flash.onHover()) {
+                                flashBinding = e.key.keysym.sym;
+                                flashKeyString[0] = toupper(flashBinding);
+                                setting["keybinding"]["flash"][0] = skillBinding;
+                                flashKey.loadFromRenderedText(flashKeyString, Arial,
+                                                              {0xff, 0xff, 0xff});
+                                settingFile.write(setting);
+                            }
+                            if (e.key.keysym.sym == SDLK_ESCAPE) {
+                                stage = MENU;
+                            };
+                        }
                     }
+                    background.render({0, 0, WINDOW_WIDTH, WINDOW_HEIGHT});
+                    settingFrame.render({86, 131, 852, 380});
+                    showGUIBtn.render();
+                    text.render({147, 170, text.getWidth() / 4, text.getHeight() / 4});
+                    ui.render({790, 170, ui.getWidth() / 4, ui.getHeight() / 4});
+                    menu.render();
+                    skill.render();
+                    skillKey.render({260, 400, 33, 42});
+                    flash.render();
+                    flashKey.render({507, 400, 32, 42});
                     SDL_RenderPresent(gRenderer);
                 }
             }
